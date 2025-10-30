920c23d3e69526fbbc58a868e0a6a16b927c5c2d
dylancordoni <dylancordoni@gmail.com>
2025-10-29 17:06:23 -0700

    fixed tip distribution, added total tips to results, and working on printing

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -6,8 +6,7 @@ document.getElementById('calculate').addEventListener('click', function() {
     const cashTips = document.getElementById('cash-tips').value ? document.getElementById('cash-tips').value : 0;
     const appTips = document.getElementById('app-tips').value ? document.getElementById('app-tips').value : 0;
     
-
-    const totalTips = parseInt(creditTips) + parseInt(cashTips) + parseInt(appTips);
+    const totalTips = parseFloat(creditTips) + parseFloat(cashTips) + parseFloat(appTips);
     const adjustedServerInfo = {};
     servers.forEach((server, index) => {
         const info = server.children;   //these are the 'label-input's
@@ -15,13 +14,18 @@ document.getElementById('calculate').addEventListener('click', function() {
             adjustedServerInfo[info[0].querySelector('input').value] = info[1].querySelector('input').value * (info[2].querySelector('input').value / 100);
         }
     });
-
-    const kitchenTips = totalTips * .2;
+    
+    const kitchenTips = parseFloat(totalTips * .2).toFixed(2);
     const serverTips = totalTips - kitchenTips;
 
-    const payoutsDiv = document.getElementById("output");
+    const date = document.getElementById("date")
+    const payoutsDiv = document.getElementById("tip-output");
+    const tipReport = document.getElementById("tip-report");
 
-    payoutsDiv.innerHTML = currentDate;
+    tipReport.innerHTML = "";
+    payoutsDiv.innerHTML = "";
+
+    date.innerHTML = `${currentDate}`;
 
     let totalHours = 0;
 
@@ -38,6 +42,29 @@ document.getElementById('calculate').addEventListener('click', function() {
     payoutsDiv.innerHTML += `
         <p>Kitchen tips: ${kitchenTips}</p>
     `;
+
+    if(creditTips && creditTips != 0) {
+        tipReport.innerHTML += `
+        <p>Credit Tips: ${creditTips}</p>
+        `
+    }
+
+    if(cashTips && cashTips != 0) {
+        tipReport.innerHTML += `
+        <p>Cash Tips: ${cashTips}</p>
+        `
+    }
+
+    if(appTips && appTips != 0) {
+        tipReport.innerHTML += `
+        <p>App Tips: ${appTips}</p>
+        `
+    }
+
+    tipReport.innerHTML += `
+    <p>Total Tips: ${totalTips}</p>
+    `
+
 
     const printButton = document.getElementById('print');
     printButton.style.visibility = 'visible';
--- scripts/print.js
+++ scripts/print.js
@@ -1,10 +1,10 @@
 document.getElementById('print').addEventListener('click', function() {
     const printContent = document.getElementById('output');
     const printWindow = window.open('','', 'height=600, width=800');
-    printWindow.document.write('<html><head><title>Print</title></head><body>');
+    printWindow.document.write('<html><head><link rel="stylesheet" href="print.css" media="print"><title>Print</title></head><body>');
     printWindow.document.write(printContent.innerHTML);
     printWindow.document.write('</body></html>');
-    printWindow.document.close();
+    //printWindow.document.close();
     printWindow.focus();
     printWindow.print();
     printWindow.close();
