b5f8e83b396006fb6c896599a2895e0806f3f532
dylancordoni <dylancordoni@gmail.com>
2025-10-29 17:17:24 -0700

    need to test out of VM

--- scripts/print.js
+++ scripts/print.js
@@ -1,11 +1,14 @@
 document.getElementById('print').addEventListener('click', function() {
     const printContent = document.getElementById('output');
     const printWindow = window.open('','', 'height=600, width=800');
-    printWindow.document.write('<html><head><link rel="stylesheet" href="print.css" media="print"><title>Print</title></head><body>');
+    printWindow.document.write('<html><head><link rel="stylesheet" href="resources/print.css" media="print"><title>Print</title></head><body>');
     printWindow.document.write(printContent.innerHTML);
     printWindow.document.write('</body></html>');
-    //printWindow.document.close();
+    printWindow.document.close();
     printWindow.focus();
-    printWindow.print();
+    setTimeout(() => {
+        printWindow.print();
+    }, 5000);
+    //printWindow.print();
     printWindow.close();
 });
